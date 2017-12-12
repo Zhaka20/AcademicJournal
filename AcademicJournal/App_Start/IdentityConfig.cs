9110f2f07f7111548646e2ea5bb841f2e80956c1
Olzhas_Kashkynbekov <Olzhas_Kashkynbekov@EPKZKARW0269>
2017-12-12 10:40:01 +0600

    Move models and dbContext to DAL

--- AcademicJournal/App_Start/IdentityConfig.cs
+++ AcademicJournal/App_Start/IdentityConfig.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using AcademicJournal.Models;
+using AcademicJournal.ViewModels;
 
 namespace AcademicJournal
 {
--- AcademicJournal/ViewModels/AccountViewModels.cs
+++ AcademicJournal/ViewModels/AccountViewModels.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace AcademicJournal.Models
+namespace AcademicJournal.ViewModels
 {
     public class ExternalLoginConfirmationViewModel
     {
